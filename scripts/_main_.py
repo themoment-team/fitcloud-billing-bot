0580f17c345598579ec87686537f67dbc6886878
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-22 18:11:03 +0900

    Add chrome_option

--- scripts/_main_.py
+++ scripts/_main_.py
@@ -13,7 +13,13 @@ password = os.environ.get('PASSWORD')
 
 url_mainpage = "https://aws.fitcloud.co.kr/dashboard"
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--single-process")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 driver.implicitly_wait(10)
 
 driver.get(url_mainpage)
