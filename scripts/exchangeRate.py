711ec6612dbe9790713ebd84daa7a024d0e55a6b
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-23 19:04:32 +0900

    Remove unused code

--- scripts/exchangeRate.py
+++ scripts/exchangeRate.py
@@ -11,10 +11,7 @@ res = requests.get(page)
 soup = BeautifulSoup(res.text, "html.parser")
 KRW = round(float(soup.select_one("div.head_info > span.value").text.replace(",","")))
 
-print(round(KRW))
-
-data = {}
-data['KRW'] = KRW
+data = { "KRW": KRW }
 
 with open(file_path, 'w') as outfile:
     json.dump(data, outfile, indent=2)
