2fc7a68c14dee7a34b933ce5f81ec4a6a8e32a4a
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-23 16:07:13 +0900

    Add calculate

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const { PythonShell } = require("python-shell");
+const { KRW } = require("./exchangeRate.json");
 require("dotenv").config();
 
 const client = new Client({
@@ -23,9 +24,13 @@ client.on("messageCreate", async (message) => {
   if (message.content === "!이번 달 요금") {
     PythonShell.run("_main_.py", option, (err, data) => {
       if (err) throw err;
-      message.reply(`이번 달 사용 금액은 ${data[0]}입니다.`);
+      message.reply(`이번 달 사용 금액은 ${calculate(data[0])}원 입니다.`);
     });
   }
 });
+
+const calculate = (dollar) => {
+  return Math.floor(dollar.split("$")[1] * KRW).toLocaleString("ko-KR");
+};
 
 client.login(process.env.DISCORD_BOT_TOKEN);
