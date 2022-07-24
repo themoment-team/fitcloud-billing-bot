b757ac255e498026770411e321df8b5f555912af
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-25 02:30:47 +0900

    Add gcs order

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const { KRW } = require("./exchangeRate.json");
 const { FEE } = require("./nowFee.json");
+const request = require("request");
 require("dotenv").config();
 
 const client = new Client({
@@ -19,6 +20,13 @@ client.on("ready", () => {
 client.on("messageCreate", async (message) => {
   if (message.content.replace(/ /g, "") === "!이번달요금") {
     message.reply(`이번 달 사용 금액은 ${calculate()}원 입니다.`);
+  }
+  if (message.content == "!gcs") {
+    request({
+      uri: "https://p94a7zofw0.execute-api.ap-northeast-2.amazonaws.com/HiRecruit-API-stage/hirecruit-github-action-caches-for-repository",
+      method: "GET",
+    });
+    message.reply("hr-action-cache-storage-stat");
   }
 });
 
