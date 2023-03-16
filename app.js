8f88b6ac1ae743ef62bcaefefe007efe8c0a7cbb
hyeongrok7874 <khr5k0228@gmail.com>
2023-03-16 13:56:17 +0900

    Update app

--- app.js
+++ app.js
@@ -12,15 +12,23 @@ const client = new Client({
   ],
 });
 
-client.on("ready", () => {
+client.on("ready", async () => {
   console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on("messageCreate", async (message) => {
-  if (message.content.replace(/ /g, "") === "!이번달요금") {
-    message.reply(`이번 달 사용 금액은 ${calculate()}원 입니다.`);
+  try {
+    const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID);
+    await channel.send(`이번 달 사용 금액은 ${calculate()}원 입니다.`);
+    process.exit();
+  } catch (e) {
+    process.exit();
   }
 });
+
+// deprecated
+// client.on("messageCreate", async (message) => {
+//   if (message.content.replace(/ /g, "") === "!이번달요금") {
+//     message.reply(`이번 달 사용 금액은 ${calculate()}원 입니다.`);
+//   }
+// });
 
 const calculate = () => {
   return Math.floor(FEE * KRW).toLocaleString("ko-KR");
