f9a3002c4e30440bdaa2c2090e6c51dae8f6492a
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-30 09:26:28 +0900

    Roll Back

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const { KRW } = require("./exchangeRate.json");
 const { FEE } = require("./nowFee.json");
-const https = require("https");
+
 require("dotenv").config();
-
-const dateFormatting = (date) => {
-  return date < 10 ? `0${date}` : date;
-};
-
-const date = new Date();
-const year = date.getFullYear();
-const month = dateFormatting(date.getMonth() + 1);
-const day = dateFormatting(date.getDate());
-
-const yesterday = `${year}${month}${day - 1}`;
-
-const getExchangeRateURL = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${process.env.KOREA_EXIM_API_KEY}&searchdate=${yesterday}&data=AP01`;
 
 const client = new Client({
   intents: [
@@ -28,11 +16,8 @@ const client = new Client({
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user.tag}!`);
   try {
-    const exchangeRate = await getExchangeRate();
     const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID);
-    await channel.send(
-      `이번 달 사용 금액은 ${calculate(FEE, exchangeRate)}원 입니다.`
-    );
+    await channel.send(`이번 달 사용 금액은 ${calculate()}원 입니다.`);
     process.exit();
   } catch (e) {
     process.exit();
@@ -46,29 +31,8 @@ client.on("ready", async () => {
 //   }
 // });
 
-const calculate = (fee, krw) => {
-  return Math.floor(fee * krw).toLocaleString("ko-KR");
+const calculate = () => {
+  return Math.floor(FEE * KRW).toLocaleString("ko-KR");
 };
 
 client.login(process.env.DISCORD_BOT_TOKEN);
-
-const getExchangeRate = () => {
-  return new Promise(function (resolve, reject) {
-    https
-      .get(getExchangeRateURL, (res) => {
-        let data = "";
-
-        res.on("data", (chunk) => {
-          data += chunk;
-        });
-
-        res.on("end", () => {
-          const returnData = JSON.parse(data);
-          resolve(returnData[returnData.length - 1].bkpr.replace(",", ""));
-        });
-      })
-      .on("error", (err) => {
-        reject(err);
-      });
-  });
-};
